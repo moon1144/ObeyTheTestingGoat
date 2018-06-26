87a3a1f36eb604ea47f39552e2d08a6bf7f01576
moon1144 <moon1144@gmail.com>
2018-06-26 23:07:31 +0200

    First unit test and url mapping, dummy view.

--- functional_tests.py
+++ functional_tests.py
@@ -26,7 +26,7 @@ class NewVisitorTest(unittest.TestCase):
 # "1: Buy peacock feathers" as an item in a to-do list
 
 # There is still a text box inviting her to add another item. She
-# enters "Use peacock feathers to make a fly" (Edith is very methodical)py
+# enters "Use peacock feathers to make a fly" (Edith is very methodical)
 
 # The page updates again, and now shows both items on her list
 
