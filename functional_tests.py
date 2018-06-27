a0155c473211d8f1beecdfd83eda59e087c48f67
moon1144 <moon1144@gmail.com>
2018-06-27 21:50:16 +0200

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -19,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
 # She notices the page title and header mention to-do lists
         self.assertIn('To-Do',self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-do',header_text)
+        self.assertIn('To-Do',header_text)
 # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
