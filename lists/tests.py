9bfc84a5092dec8ceb4d060f08db426f3c5dacb7
moon1144 <moon1144@gmail.com>
2018-07-02 17:19:37 +0200

    Switch to using a redirect, test for redirects

--- lists/tests.py
+++ lists/tests.py
@@ -15,10 +15,22 @@ class HomePageTest(TestCase):
 
     def test_can_save_a_POST_request(self):
         response = self.client.post('/',data={'item_text':'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
 
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertEqual(Item.objects.count(),1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
 
+        self.assertEqual(response.status_code,302)
+        self.assertEqual(response['location'],'/')
+
+        # self.assertIn('A new list item', response.content.decode())
+
+        # self.assertTemplateUsed(response, 'home.html')
+
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(),0)
 class ItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
@@ -37,3 +49,4 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text,"The first (ever) list item")
         self.assertEqual(second_saved_item.text,"Item the second")
+
