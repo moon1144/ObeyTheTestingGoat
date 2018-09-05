d6222698472d073f7eb5a15b52fbfa066f281ee3
moon1144 <moon1144@gmail.com>
2018-09-05 15:11:38 +0200

    Split out unit tests into two files

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -4,6 +4,7 @@ from fabric.api import cd, env, local, run
 
 REPO_URL = 'https://github.com/moon1144/ObeyTheTestingGoat.git'
 
+
 def deploy():
     site_folder = f'/home/{env.user}/sites/{env.host}'
     run(f'mkdir -p {site_folder}')
@@ -14,6 +15,7 @@ def deploy():
         _update_static_files()
         _update_database()
 
+
 def _get_latest_source():
     if exists('.git'):
         run('git fetch')
@@ -22,10 +24,12 @@ def _get_latest_source():
     current_commit = local("git log -n 1 --format=%H", capture=True)
     run(f'git reset --hard {current_commit}')
 
+
 def _update_virtualenv():
     if not exists('virtualenv/bin/pip'):
         run(f'python3.6 -m venv virtualenv')
     run('./virtualenv/bin/pip install -r requirements.txt')
+
 
 def _create_or_update_dotenv():
     append('.env', 'DJANGO_DEBUG_FALSE=y')
@@ -40,6 +44,7 @@ def _create_or_update_dotenv():
 
 def _update_static_files():
     run('./virtualenv/bin/python manage.py collectstatic --noinput')
+
 
 def _update_database():
     run('./virtualenv/bin/python manage.py migrate --noinput')
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -18,34 +18,6 @@ class HomePageTest(TestCase):
         self.assertEqual(Item.objects.count(), 0)
 
 
-class ListAndItemModelTest(TestCase):
-
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = "The first (ever) list item"
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = "Item the second"
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, "The first (ever) list item")
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, "Item the second")
-        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
@@ -95,11 +67,11 @@ class NewListTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new item for existing list')
         self.assertEqual(new_item.list, correct_list)
-    #
-    # def test_redirects_after_POST(self):
-    #     response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
-    #     new_list = List.objects.first()
-    #     self.assertRedirects(response, f'/lists/{new_list.id}/')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        new_list = List.objects.first()
+        self.assertRedirects(response, f'/lists/{new_list.id}/')
 
     def test_redirects_to_list_view(self):
         other_liest = List.objects.create()
